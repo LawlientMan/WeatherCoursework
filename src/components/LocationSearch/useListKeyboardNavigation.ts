5e695cbe8124a0d2c97ff7ca40ccd9aa26e74a21
Pavel Kryvasheyeu <kryvasheyeu.pavel@gmail.com>
2024-03-06 10:29:23 +0400

    Refactoring keyhandler

--- src/components/LocationSearch/useListKeyboardNavigation.ts
+++ src/components/LocationSearch/useListKeyboardNavigation.ts
@@ -1,64 +1,46 @@
 import { Dispatch, useState } from "react";
 
-export const useListKeyboardNavigation = (countItems: number | null): 
-[(e: React.KeyboardEvent) => void, number, React.Dispatch<React.SetStateAction<number>>] => {
+export const useListKeyboardNavigation = (countItems: number | null):
+    [(e: React.KeyboardEvent) => void, number, React.Dispatch<React.SetStateAction<number>>] => {
     const [activeOption, setActiveOption] = useState<number>(-1);
-    console.log("countItems" + countItems)
 
     const onKeyDown = (e: React.KeyboardEvent) => {
         if (!countItems || countItems == 0) return;
 
-        console.log(" data.length" + countItems)
         if (e.key === 'ArrowUp') {
-            console.log(" ArrowUp" + activeOption)
-
             e.preventDefault();
-            if (activeOption <= 0)
-                return;
-            setActiveOption(activeOption - 1);
 
-            console.log(" ArrowUp2" + activeOption)
-
-
-        } else if (e.key === 'ArrowDown') {
-            console.log(" ArrowDown" + activeOption)
-
-            e.preventDefault();
-            if (activeOption >= countItems - 1)
-                return;
-            setActiveOption(activeOption + 1);
-
-            console.log(" ArrowDown2" + activeOption)
-        } else if (e.key === "Tab" && e.shiftKey) {
-
-            if (activeOption <= 0) {
-                setActiveOption(countItems - 1);
-            }
-            else {
+            if (activeOption > 0) {
                 setActiveOption(activeOption - 1);
             }
-
-            console.log(" Tab and shift" + activeOption)
-
+        } else if (e.key === 'ArrowDown') {
             e.preventDefault();
-        } else if (e.key === "Tab") {
 
-            if (activeOption >= countItems - 1) {
-                setActiveOption(0);
-            }
-            else {
+            if (activeOption < countItems - 1) {
                 setActiveOption(activeOption + 1);
             }
-
-            console.log(" Tab" + activeOption)
-
+        } else if (e.key === "Tab") {
             e.preventDefault();
+
+            if (e.shiftKey) {
+                if (activeOption <= 0) {
+                    setActiveOption(countItems - 1);
+                }
+                else {
+                    setActiveOption(activeOption - 1);
+                }
+            }
+            else {
+                if (activeOption >= countItems - 1) {
+                    setActiveOption(0);
+                }
+                else {
+                    setActiveOption(activeOption + 1);
+                }
+            }
         } else if (e.key === "Escape") {
+            e.preventDefault();
             setActiveOption(-1);
-            console.log(" Esc" + activeOption)
-            e.preventDefault();
-        } else if (e.shiftKey) {
-            e.preventDefault();
         }
     };
 
